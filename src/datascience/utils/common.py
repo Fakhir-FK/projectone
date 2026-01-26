542a82bb2e7fb72b17e0705336ef6d38f11b8437
Ali Khan <alikhan680880@gmail.com>
2026-01-26 22:22:45 +0500

    Executed data ingestion.ipynb

--- src/datascience/utils/common.py
+++ src/datascience/utils/common.py
@@ -30,7 +30,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
             logger.info(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
     except BoxValueError:
-        raise ValueError("yaml file is empty")
+        raise ValueError(f"yaml file is empty")
     except Exception as e:
         raise e
         
